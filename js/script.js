a5e6ffb041948a97aeab0ed57ebad73d5fc8fa76
Ronald Macmaster <ronnymacmaster@gmail.com>
2018-02-27 22:36:35 -0500

    some js. need to commit this in prep for the new php study

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log("hello node!")
+console.log("hello node!");
+console.log(42 + "a");
+console.log(String(42));
+var json = JSON.stringify( [1, undefined, function(){}, 4] );
+console.log(json);
+
+// test the truth of null
+if (null) {
+  console.log("null is true!");
+} else {
+  console.log("null is false!");
+}
