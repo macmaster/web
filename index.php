20e4410395a97be1a69e00816205db2d7b6ce0b7
Ronald Macmaster <ronnymacmaster@gmail.com>
2018-03-03 01:54:08 -0500

    nodejs dependencies

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <img id="banner" src="img/cluck.png">
 
     <div id="greeting">
-    <p>I love Paris in the the springtime.</p>
+      <p>I love Paris in the the springtime.</p>
     </div>
 
     <div id="nav-bar">
@@ -17,7 +17,7 @@
       <a href="https://nytimes.com/">nytimes.com</a>
       <a href="https://youtube.com/">youtube.com</a>
     </div>
-    
+
     <iframe id="map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12088.076025268982!2d-73.8380756!3d40.7616067!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2c054b5606e54f7d!2sNew+World+Mall!5e0!3m2!1sen!2sus!4v1514821926256" frameborder="0" allowfullscreen></iframe>
     <img id="flag" src="img/korea.png">
     <div id="footer">©2018 Cluck</div>
--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
-console.log("hello node!");
-console.log(42 + "a");
-console.log(String(42));
-var json = JSON.stringify( [1, undefined, function(){}, 4] );
-console.log(json);
-
-// test the truth of null
-if (null) {
-  console.log("null is true!");
-} else {
-  console.log("null is false!");
-}
+const popper = require('popper.js');
+const jquery = require('jquery');
+const bootstrap = require('bootstrap');
