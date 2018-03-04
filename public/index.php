3da6494b33bf256c96eb2348c5ca5c4bd9bcb8a4
Ronald Macmaster <ronnymacmaster@gmail.com>
2018-03-04 00:59:44 -0500

    refactor div elements to php for loop

--- public/index.php
+++ public/index.php
@@ -12,10 +12,15 @@
     </div>
     <div id="button"></div>
     <div id="nav-bar">
-      <div class="nav-button" href="https://google.com/">google.com</div>
-      <div class="nav-button" href="https://mozilla.org/">mozilla.org</div>
-      <div class="nav-button" href="https://nytimes.com/">nytimes.com</div>
-      <div class="nav-button" href="https://youtube.com/">youtube.com</div>
+      <?php
+        $domains = [ 
+          "google.com", 
+          "mozilla.org", 
+          "nytimes.com", 
+          "youtube.com", 
+        ];
+        foreach ($domains as $domain) echo "<div class='nav-button' href='http://$domain'>$domain</div>";
+      ?>
     </div>
 
     <iframe id="map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12088.076025268982!2d-73.8380756!3d40.7616067!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2c054b5606e54f7d!2sNew+World+Mall!5e0!3m2!1sen!2sus!4v1514821926256" frameborder="0" allowfullscreen></iframe>
