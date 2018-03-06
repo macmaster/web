698fc7bafeaf13af7b7a717ced2f705ee0e6eb91
Ronald Macmaster <ronnymacmaster@gmail.com>
2018-03-06 01:08:38 -0500

    refactor react components to prototypical delegation

--- src/ClockButton.js
+++ src/ClockButton.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Button from "material-ui/Button";
 
-export default function Clock() {
+Object.setPrototypeOf(Clock.prototype, React.Component.prototype);
+export default function Clock(props) {
+  React.Component(props);
+  this.state = {
+    date: new Date()
+  }
+}
+
+Clock.prototype.render = function render() {
   return (
     <Button variant="raised" color="primary">
-      {new Date().toUTCString()}
+      {this.state.date.toUTCString()}
     </Button>
   );
 }
-
--- src/NavButton.js
+++ src/NavButton.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Button from 'material-ui/Button';
 
-export default function NavButton (props) {
+Object.setPrototypeOf(NavButton.prototype, React.Component.prototype);
+export default function NavButton(props) {
+  React.Component(props);
+  this.state = {
+    href: props.href,
+    hostname: new URL(props.href).hostname
+  }
+}
+
+NavButton.prototype.render = function render() {
   return (
-    <Button variant="raised" color="primary" href={props.href}>
-      {new URL(props.href).hostname}
+    <Button variant="raised" color="primary" href={this.state.href}>
+      {this.state.hostname}
     </Button>
   );
 }
